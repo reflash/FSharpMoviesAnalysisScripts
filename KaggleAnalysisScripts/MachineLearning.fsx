c32aa787a88073a582116401a8443120a706266b
reflash <reflash-ne@yandex.ru>
2016-10-04 19:20:10 +0300

    Added machine learning with regression

--- KaggleAnalysisScripts/MachineLearning.fsx
+++ KaggleAnalysisScripts/MachineLearning.fsx
@@ -1,5 +1,5 @@
-﻿#load "C:\Users\daniil.ekzaryan\Documents\Visual Studio 2015\Projects\KaggleAnalysisScripts\KaggleAnalysisScripts\CSVtoMongoStep.fsx"
-#load "C:\Users\daniil.ekzaryan\Documents\Visual Studio 2015\Projects\KaggleAnalysisScripts\KaggleAnalysisScripts\MongotoDataStepWithPreparations.fsx"
+﻿#load "C:\Users\daniil.ekzaryan\Documents\Visual Studio 2015\Projects\KaggleAnalysisScripts\KaggleAnalysisScripts\StepCSVtoMongo.fsx"
+#load "C:\Users\daniil.ekzaryan\Documents\Visual Studio 2015\Projects\KaggleAnalysisScripts\KaggleAnalysisScripts\StepMongotoDataWithPreparations.fsx"
 
 #r "C:\Users\daniil.ekzaryan\Documents\Visual Studio 2015\Projects\KaggleAnalysisScripts\packages/Accord.3.3.0/lib/net45/Accord.dll"
 #r "C:\Users\daniil.ekzaryan\Documents\Visual Studio 2015\Projects\KaggleAnalysisScripts\packages/Accord.Math.3.3.0/lib/net45/Accord.Math.dll"
@@ -22,11 +22,6 @@ let getMoviesData1 movies =
         let outputs = (movies |> Seq.map (fun m -> m.Score) |> Seq.toArray)
         { Predictors = inputs; Outputs = outputs }
 
-let getMoviesData2 movies = 
-        let inputs = movies |> Seq.map (fun m -> ([| float (Option.get m.Budget); float m.Likes |])) |> Seq.toArray
-        let outputs = (movies |> Seq.map (fun m -> m.Score > 6.5) |> Seq.toArray)
-        { BoolPredictors = inputs; BoolOutputs = outputs }
-
 let getMultipleRegression data =
     let regression = new OrdinaryLeastSquares()
     regression.Learn(data.Predictors, data.Outputs)
@@ -39,6 +34,11 @@ moviesWithBudget
  |> getMultipleRegression 
  |> computeByMultipleLinearRegression [| 237000000.0; 48350.0 |]
 
+
+let getMoviesData2 movies = 
+        let inputs = movies |> Seq.map (fun m -> ([| float (Option.get m.Budget); float m.Likes |])) |> Seq.toArray
+        let outputs = (movies |> Seq.map (fun m -> m.Score > 6.5) |> Seq.toArray)
+        { BoolPredictors = inputs; BoolOutputs = outputs }
 
 let getLogisticRegression data =
     let learner = new IterativeReweightedLeastSquares<LogisticRegression>()
